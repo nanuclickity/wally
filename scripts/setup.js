8ecf69f3950cb68c6546bc0762a4426216fa8e73
Tushar Kant <nanu.clickity@gmail.com>
2019-04-20 17:10:29 +0530

    setup changes

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,18 +1,54 @@
-const fs = require('fs')
+const os = require('os')
 const path = require('path')
+const chalk = require('chalk')
+const fs = require('fs').promises
+const Utils = require('../lib/utils')
+const getDB = require('../lib/db')
 
-const currentDir = process.cwd()
-const ENV_FILE = path.join(currentDir, '.env')
-const CACHE_FILE = path.join(currentDir, 'cache.json')
+async function getPicturesDirectory () {
+  const osPicturesDir = path.join(os.homedir(), './Pictures')
+  const picturesDirExists = await Utils.exists(osPicturesDir)
 
-function setup () {
-  if (!fs.existsSync(ENV_FILE)) {
-    fs.copyFileSync(path.join(ENV_FILE, '../.env.sample'), ENV_FILE)
-    console.log('created .env')
+  if (!picturesDirExists) {
+    console.log(
+      chalk.yellow(
+        `Could not find your pictures directory at: ${osPicturesDir}` +
+          `\nplease manually set wally downloads dir by running:` +
+          `\n  ${chalk.white('wally set dir <path_to_dir>')}`
+      )
+    )
+
+    return undefined
   }
-  if (!fs.existsSync(CACHE_FILE)) {
-    fs.writeFileSync(CACHE_FILE, JSON.stringify([]), 'utf-8')
-    console.log('created cache.json')
+
+  const wallyDir = path.join(osPicturesDir, './Wally')
+  const wallyDirExists = await Utils.exists(wallyDir)
+  if (!wallyDirExists) {
+    await fs.mkdir(wallyDir)
+    console.log(chalk.green(`Created directory: ${wallyDir}`))
+  } else {
+    console.log(chalk.white(`Found wally dir at: ${wallyDir}`))
+  }
+
+  return wallyDir
+}
+
+async function setup () {
+  try {
+    const dir = await getPicturesDirectory()
+    const dbDefaults = {
+      config: {
+        dir
+      },
+      cache: []
+    }
+
+    const db = await getDB()
+    await db.defaults(dbDefaults).write()
+    console.log('Setup OK')
+  } catch (err) {
+    console.log(chalk.red(`[SETUP_ERR] ${err.message}`))
+    console.error(err)
   }
 }
 
